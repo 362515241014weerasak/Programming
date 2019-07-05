4dd28907dee45c44ca88eced85d12c77a9b5297b
362515241014weerasak <52289590+362515241014weerasak@users.noreply.github.com>
2019-07-05 14:06:00 +0700

    Add files via upload
    โปรแกรมแรก

---
+++ First Time.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+print("one mouth year Shop")
+print("------------------------------------------")
+print(" 1. Water               x1 :  10 THB")
+print(" 2. Lay                  x3 :   30 THB")
+print(" 3. Icecream          x3 :   30 THB")
+print("------------------------------------------")
+print(" Total                        :   70 THB")
+print(type(10))
+print(type("Hello"))
+print(type(True))
+print(type(10.3))
+print(10+3.5)
+
+
+# In[ ]:
+
+
+
+
