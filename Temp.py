d21b7adf8bb1d6da85ba502c77780cd53fd100a8
362515241014weerasak <52289590+362515241014weerasak@users.noreply.github.com>
2019-08-17 23:09:21 +0700

    Add files via upload

---
+++ Temp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+C = float(input("Fill out C : ") )
+F = ((9/5) * C) + 32
+K = C +  273.15
+print(F, "F")
+print(K, "K")
+
+
+# In[ ]:
+
+
+
+
