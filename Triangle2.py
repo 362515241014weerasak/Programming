fb55ff8558de413d455912a619aa12d35508677b
362515241014weerasak <52289590+362515241014weerasak@users.noreply.github.com>
2019-08-18 13:56:25 +0700

    Add files via upload

---
+++ Triangle2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+#นาย วีระศักดิ์ วงษ์ภู่งาม 362515241014 EE36241N
+import math
+a = float(input("Fill Out a :"))
+b = float(input("Fill Out b :"))
+D = float(input("Fill Out D :"))
+C = D*math.pi/180
+c = math.sqrt((a*a)+(b*b)-(2*a*b*math.cos(C)))
+print("c =", c, "cm.")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
