bad28a2bffacab6a99c03c29734de7f82bdfe7bd
362515241014weerasak <52289590+362515241014weerasak@users.noreply.github.com>
2019-08-31 23:44:28 +0700

    Update Triangle3.py

--- Triangle3.py
+++ Triangle3.py
@@ -6,14 +6,14 @@
 
 # นาย วีระศักดิ์ วงษ์ภู่งาม 362515241014 EE36241N
 max = 0
-i = 3
+i = 3 
 sum = 0
 for i in range(i):
+    angle = int(input("enter angle :"))
     if angle > max:
         max = angle
-    angle = int(input("enter angle :"))
     sum = sum + angle
-print("sum :", sum)
+print("perimeter :", sum)
 print("hypotenuse", max)
         
     
