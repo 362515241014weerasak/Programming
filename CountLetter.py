2197001165f5d70dea16651a6288fd0349e164b6
362515241014weerasak <52289590+362515241014weerasak@users.noreply.github.com>
2019-09-06 21:50:44 +0700

    Update CountLetter.py

--- CountLetter.py
+++ CountLetter.py
@@ -1,9 +1,6 @@
 # นาย วีระศักดิ์ วงษ์ภู่งาม 362515241014 EE36241N
-string=input("Enter string:")
-count1=0
-count2=0
-for i in string:
-      if(i.isupper()):
-            count2=count2+1
-print("The number of capital letter is:")
-print(count2)
+n=0
+for w in input().strip() :
+    if w.isupper():
+        n+=1
+print(n)
